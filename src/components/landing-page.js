3450a461b80b9b29c0242e43f50e025f5863e3c0
Molly Elfers <elfers.molly@gmail.com>
2019-04-22 11:20:52 -0700

    CSS changes for mobile-first design

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function LandingPage() {
-    return(
-        <div>
-            <h1 id="landing-header">Nom List</h1>
-            <h4>An app that helps you track and rate restaurant visits</h4>
-            <img src={require("../images/donut.png")} id="main-donut" alt=" " />
-            <Link to='/auth/login'>
-                <button>Log In</button>
-            </Link>
-            <Link to='/auth/signup'>
-                <button>Sign Up</button>
-            </Link>
-        </div>
-    );
+  return (
+    <div>
+      <h1 id="landing-header">Nom List</h1>
+      <h4>
+        An app that allows you to search for local restaurants, and create lists
+        of places you'd like to go, or have already visited!
+      </h4>
+      <img src={require("../images/donut.png")} id="main-donut" alt=" " />
+      <Link to="/auth/login">
+        <button>Log In</button>
+      </Link>
+      <Link to="/auth/signup">
+        <button>Sign Up</button>
+      </Link>
+    </div>
+  );
 }
